7bce7e3b5e4da6b6e43e23183315bd38351ef960
seregapanteleev <seregapanteleev@mail.ru>
2021-03-23 11:38:14 +0300

    new

--- junit/src/main/java/junit/Calculator.java
+++ junit/src/main/java/junit/Calculator.java
@@ -2,4 +2,16 @@ package junit;
 
 public class Calculator {
 
+	public static int getSum(int x, int y) {
+		return x + y;
+	}
+
+	public static int getDivide(int x, int y) {
+		return x - y;
+	}
+
+	public static int getMultiple(int x, int y) {
+		return x * y;
+	}
+
 }
